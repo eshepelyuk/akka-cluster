c43c4892d1e23b96231e8589fbc8ce3daa1d5bf6
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2017-09-04 21:32:54 +0300

    Fixes

--- wallet_rest/src/main/scala/com/newagesol/wallet_rest/App.scala
+++ wallet_rest/src/main/scala/com/newagesol/wallet_rest/App.scala
@@ -1,6 +1,7 @@
 package com.newagesol.wallet_rest
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorPath, ActorSystem}
+import akka.cluster.client.{ClusterClient, ClusterClientSettings}
 import akka.cluster.sharding.{ClusterSharding, ShardRegion}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes.InternalServerError
@@ -8,8 +9,10 @@ import akka.http.scaladsl.server.Directives._
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import com.lightbend.constructr.coordination.zookeeper.ZookeeperCoordination
 import de.heikoseeberger.constructr.ConstructrExtension
 
+import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 import scala.util.{Failure, Success}
 
@@ -29,15 +32,16 @@ object App extends App {
   implicit val actorSystem = ActorSystem.create("WalletActorSystem")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = actorSystem.dispatcher
+  implicit val timeout = Timeout(15.seconds)
 
-  //  val zk = new ZookeeperCoordination("WalletActorSystem", actorSystem)
-  //
-  //  val actors: Set[ActorPath] = Await.result(zk.getNodes(), 15.seconds).map(addr => {
-  //    ActorPath.fromString(s"$addr/system/receptionist")
-  //  })
-  //
-  //  val clusterClient = actorSystem.actorOf(ClusterClient.props(ClusterClientSettings.create(actorSystem)
-  //    .withInitialContacts(actors)), "walletClient")
+  val zk = new ZookeeperCoordination("WalletActorSystem", actorSystem)
+
+  val actors: Set[ActorPath] = Await.result(zk.getNodes(), 15.seconds).map(addr => {
+    ActorPath.fromString(s"$addr/system/receptionist")
+  })
+
+  val clusterClient = actorSystem.actorOf(ClusterClient.props(ClusterClientSettings.create(actorSystem)
+    .withInitialContacts(actors)), "walletClient")
 
   ConstructrExtension(actorSystem)
 
@@ -49,22 +53,19 @@ object App extends App {
   val route =
     path("hello_shard" / Remaining) { w =>
       get {
-        implicit val timeout = Timeout(15.seconds)
         onComplete(walletProxy ? w) {
           case Success(x) => complete(s"$x")
           case Failure(ex) => complete(InternalServerError, s"${ex.getMessage}")
         }
       }
+    } ~ path("hello_client" / Remaining) { w =>
+      get {
+        onComplete(clusterClient ? ClusterClient.Send("/system/sharding/wallet", w, localAffinity = false)) {
+          case Success(x) => complete(s"$x")
+          case Failure(ex) => complete(InternalServerError, s"${ex.getMessage}")
+        }
+      }
     }
-  //      ~
-  //    path("hello_client" / Remaining) { w =>
-  //      get {
-  //        onComplete(clusterClient ? ClusterClient.Send("/system/sharding/wallet", w, localAffinity = false)) {
-  //          case Success(x) => complete(s"$x")
-  //          case Failure(ex) => complete(InternalServerError, s"${ex.getMessage}")
-  //        }
-  //      }
-  //    }
 
   val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 9090)
 }
